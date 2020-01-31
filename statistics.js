82b64c8a287a9b7606b29627d17128f87165d06d
Max <ma15fo43@gmail.com>
2020-01-31 18:16:55 +0100

    Initial commit

---
+++ statistics.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+const Token = require("./utilities.js");
+
+Client.on("ready", () => {
+    console.log(Client.user.username);
+    document.getElementById("usersNumber").innerHTML = `<i class="fas fa-check-square"></i> ${Client.users.size} users... and counting!`;
+    document.getElementById("serversNumber").innerHTML = `<i class="fas fa-check-square"></i> ${Client.guilds.size} guilds... and counting!`;
+    document.getElementById("channelsNumber").innerHTML = `<i class="fas fa-check-square"></i> ${Client.channels.size} channels... and counting!`;
+    document.getElementById("emojisNumber").innerHTML = `<i class="fas fa-check-square"></i> ${Client.emojis.size} emojis... and counting!`;
+});
+
+Client.login(Token.token);
